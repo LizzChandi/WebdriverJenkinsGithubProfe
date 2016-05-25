7f1eba0f44aeb1ff17cc47ed0adb27008d09a626
Gaby <gaby_r0805@hotmail.com>
2016-05-25 12:35:27 -0500

    change baseURL

--- src/test/java/VerifyTextMember2.java
+++ src/test/java/VerifyTextMember2.java
@@ -20,7 +20,7 @@ public class VerifyTextMember2 {
     public void setUp() throws Exception {
         Logger.getLogger("").setLevel(Level.SEVERE);
         driver = new HtmlUnitDriver();
-        baseUrl = "http://probando-workspacelizz096.rhcloud.com/";
+        baseUrl = String.valueOf(System.getProperty("baseURL"));
         driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
     }
 
