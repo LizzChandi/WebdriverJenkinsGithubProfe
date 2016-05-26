e22722549e913b62d01996a8fc12026bfc606f13
Lizz <lizethpa096@hotmail.com>
2016-05-26 11:48:24 +0100

    BaseURL-Modified

--- src/test/java/TestTitlePositionWorkProj.java
+++ src/test/java/TestTitlePositionWorkProj.java
@@ -28,8 +28,9 @@ public class TestTitlePositionWorkProj {
 
     @Before
     public void setUp() throws Exception {
-        driver = new FirefoxDriver();
-        baseUrl = "http://staging-workspacelizz096.rhcloud.com/";
+        Logger.getLogger("").setLevel(Level.SEVERE);
+        driver = new HtmlUnitDriver();
+        baseUrl = String.valueOf(System.getProperty("baseURL"));
         driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
     }
 
